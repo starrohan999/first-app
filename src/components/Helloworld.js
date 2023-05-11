a60115d862fb5fa6c0c9ef4a34446e03d4ac4dcc
Rohan Kumar <starrohan999@gmail.com>
2023-05-11 22:36:37 +0530

    initial files

---
+++ src/components/Helloworld.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Helloworld = () => {
+  return (
+    <div>
+    <h1>
+      hello world
+    </h1>
+    <h2>
+      Learning ReactJs
+    </h2>
+    <p>cool</p>
+   </div>
+  )
+}
+
+export default Helloworld
